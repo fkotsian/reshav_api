2dde11eedc78c8a0f0d5beccd25543654dd74ee8
Reshav Abraham <reshavabraham@gmail.com>
2021-05-07 09:56:42 -0400

    movie client class for themoviedb api

--- movie_api/core/views.py
+++ movie_api/core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import os
+import logging
 
 from movie_client import MovieClient
 
-if os.environ['API_KEY']:
+if "API_KEY" in os.environ:
     movie_client = MovieClient(os.environ['API_KEY'])
+    logging.info("Movie client initialized")
 else:
     raise ValueError("Please Set API_KEY environment variable")
     
