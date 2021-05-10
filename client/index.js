cb835af839dae06b139b44e807628b0526a9feca
Reshav Abraham <reshavabraham@gmail.com>
2021-05-10 09:02:31 -0400

    grid presentational and movie container

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '@babel/polyfill'
-import { getTopMovies } from './api/MovieApi';
 import { MovieGrid } from './components/MovieGrid';
 import { Button, TextField } from '@material-ui/core';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
@@ -23,5 +22,4 @@ function App() {
     </div>
   );
 }
-{getTopMovies()}
 ReactDOM.render(<App />, document.getElementById('root'));
