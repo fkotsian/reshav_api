964275dcb3e2f4163ca58a3ae112b6fadee5126a
Reshav Abraham <reshavabraham@gmail.com>
2021-05-09 22:53:31 -0400

    ag-grid searchbar

--- client/components/MovieGrid.js
+++ client/components/MovieGrid.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { getTopMovies } from '../api/MovieApi';
+// import TextField from '@material-ui/core/TextField';
 import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { Button, TextField } from '@material-ui/core';
 
 export const MovieGrid = () => {
-    const [movies, setMovies] = useState([{"Movie": "x.title", "Overview": "x.overview", "Release Date": "x.release_date"}]);
+    const [movies, setMovies] = useState([]);
 
     console.log("movies", movies);
     useEffect(()=> {
@@ -20,21 +23,23 @@ export const MovieGrid = () => {
         
     },[])
 
-   
-    const rowData = [
-       {make: "Toyota", model: "Celica", price: 35000},
-       {make: "Ford", model: "Mondeo", price: 32000},
-       {make: "Porsche", model: "Boxter", price: 72000}
-   ];
-
    return (
-       <div className="ag-theme-alpine" style={{height: 400, width: 900}}>
-           <AgGridReact
-               rowData={movies}>
-               <AgGridColumn field="Movie"></AgGridColumn>
-               <AgGridColumn field="Overview"></AgGridColumn>
-               <AgGridColumn field="Release Date"></AgGridColumn>
-           </AgGridReact>
+       <div>
+            <h2><b>Movie App</b></h2>
+            <div>
+                <TextField id="outlined-basic" label="Search Movies" variant="outlined" />
+                <Button onClick={()=>{console.log("Search movies")}} style={{"background-color":"purple", "margin-left": "10px", "height":"55px"}}> <ArrowForwardIosIcon /> </Button>
+            </div>
+            <br />
+            <br />
+            <div className="ag-theme-alpine" style={{height: 400, width: 900}}>
+                <AgGridReact
+                    rowData={movies}>
+                    <AgGridColumn field="Movie"></AgGridColumn>
+                    <AgGridColumn field="Overview"></AgGridColumn>
+                    <AgGridColumn field="Release Date"></AgGridColumn>
+                </AgGridReact>
+            </div>
        </div>
    );
 };
